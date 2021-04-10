aa29c4e632dd107bebea9004ace3986265e4a07b
maebashi <k5b2k9r6@gmail.com>
2021-04-10 17:13:01 +0900

    コードを修正

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import tkinter as tk
 import math
+import random
+import numpy as np
 
-value_dict = {}
 canvas = None
 
 SQUARE_LENGTH = 100
@@ -12,6 +13,7 @@ NUMBER = 4
 LENGTH = SQUARE_LENGTH * NUMBER + BORDER_WIDTH * NUMBER
 CELL_COLOR = '#cbbeb5'
 BORDER_COLOR = '#b2a698'
+PATTERNS = [[i, j] for i in range(4) for j in range(4)]
 
 def set_field():
   canvas.create_rectangle(POSITION["x"], POSITION["y"], LENGTH + POSITION["x"], LENGTH + POSITION["y"], fill='#cbbeb5', width=BORDER_WIDTH, outline=BORDER_COLOR)
@@ -36,18 +38,40 @@ def set_number(num, x, y):
   center_y = POSITION["y"] + BORDER_WIDTH * y + BORDER_WIDTH / 2 + SQUARE_LENGTH * y + SQUARE_LENGTH / 2
   canvas.create_rectangle(center_x - SQUARE_LENGTH / 2, center_y - SQUARE_LENGTH / 2, center_x + SQUARE_LENGTH / 2, center_y + SQUARE_LENGTH / 2, fill=CELL_COLOR, width=0)
   canvas.create_text(center_x, center_y, text=num, justify="center", font=("", 70), tag="count_text")
-  update_dict = {(x,y):num}
-  value_dict.update(update_dict)
+  # update_dict = {(x,y):num}
+  # value_dict.update(update_dict)
   
-  
+
+def set_array():
+  i = 0
+  j = 0
+  array = [[None for i in range(4)] for j in range(4)]
+  print(array)
+  for i, j in random.sample(PATTERNS, 2):
+    array[j][i] = random.choice([2, 4])
+  print(array)
+
+  for i in range(4):
+      for j in range(4):
+        if array[j][i] != None:
+          set_number(array[j][i], i, j)
+
+  array0 = np.array[0]
+  array1 = np.array[1]
+  array2 = np.array[2]
+  array3 = np.array[3]
+  array_all = array0 + array1 + array2 + array3
+  print(array[0])
+  print(array[1])
+  print(array[2])
+  print(array[3])
+  print(array_all)
+
 
 def operate(event):
   print(event.keysym)
   if event.keysym == "Left":
-    print(value_dict)
     canvas.delete("count_text")
-    set_number(value_dict[0,3],0,3)
-    set_number(value_dict[2,1],0,1)
 
 
   # if event.keysym == "Right":
@@ -63,12 +87,16 @@ def operate(event):
   #    set_number("2", 0, 0) 
 
 
+
+
 def play():
   global canvas
   root, canvas = create_canvas()
   set_field()
-  set_number("2", 0, 3)
-  set_number("4", 2, 1)
+  set_array()
+  # calc_array()
+  # set_number("2", 0, 3)
+  # set_number("4", 2, 1)
   root.bind("<Key>", lambda event: operate(event))
   root.mainloop()
 
